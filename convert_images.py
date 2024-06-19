f66e3d8cec83feabff274ed60555d809b63e85c0
PhobosX15 <pavan4060aakash@gmail.com>
2024-06-19 17:22:24 +0200

    Update: gitignore and convert_images file

--- convert_images.py
+++ convert_images.py
@@ -25,11 +25,11 @@ def resize_image( folder, resized_folder, output, type = "Image", new_shape = (2
                 except Exception as e:
                     print(f"Error: {e} in {file}")
                     file_num = file[-5]
-                    RADAR_NAME = "RADAR" + file_num+".png"
+                    RADAR_NAME = "RADAR" + file_num+".npy"
                     print(f"Folder: {folder}")
                     # delete the RADAR NAME in output\\ra\\folder
                     try:
-                        os.remove(os.path.join("output\\ra", folder, RADAR_NAME))
+                        os.remove(os.path.join("/data/pavan/output/ra_matrix", folder, RADAR_NAME))
                     except FileNotFoundError:
                         print(f"File not found: {RADAR_NAME}")
     
@@ -56,12 +56,13 @@ if __name__ == '__main__':
     freeze_support()
     # Do Radar first
     print("Processing RADAR")
-    output = "output\\ra_matrix"
+    new_shape = ast.literal_eval(input("Enter the new shape of the image (width, height): "))
+    output = "/data/pavan/output/ra_matrix"
     folders = [f for f in os.listdir(output) if os.path.isdir(os.path.join(output, f))]
-    resized_folder = "resized_224\\ra_matrix"
+    resized_folder = "/data/pavan/resized_384/ra_matrix"
     os.makedirs(resized_folder, exist_ok=True)
     #os.makedirs(os.path.join(resized_folder, "images"), exist_ok=True)
-    folder_to_process = [(folder, resized_folder,output,"RADAR") for folder in folders]
+    folder_to_process = [(folder, resized_folder,output,"RADAR", new_shape) for folder in folders]
     with Pool(processes= 12) as pool:
         pool.starmap(resize_image, folder_to_process)
     pool.close()
@@ -69,10 +70,10 @@ if __name__ == '__main__':
     print("Done Processing RADAR")
     # Processing Image     
     print("Processing Images")   
-    new_shape = ast.literal_eval(input("Enter the new shape of the image (width, height): "))
-    output = "output\\images"
+    #new_shape = ast.literal_eval(input("Enter the new shape of the image (width, height): "))
+    output = "/data/pavan/output/images"
     folders = [f for f in os.listdir(output) if os.path.isdir(os.path.join(output, f))]
-    resized_folder = "resized_224\\images"
+    resized_folder = "data/pavan/resized_384/images"
     os.makedirs(resized_folder, exist_ok=True)
     #os.makedirs(os.path.join(resized_folder, "images"), exist_ok=True)
     folder_to_process = [(folder, resized_folder,output,"Image",new_shape) for folder in folders]
